e877c20e0e2d48eb9a2829f1371c57deed6e6bfb
Xayah <zds1249475336@gmail.com>
2022-02-25 00:53:32 +0800

    feat: Create sample

---
+++ app/src/main/java/com/xayah/sample/materialyoufileexplorer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.xayah.sample.materialyoufileexplorer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
