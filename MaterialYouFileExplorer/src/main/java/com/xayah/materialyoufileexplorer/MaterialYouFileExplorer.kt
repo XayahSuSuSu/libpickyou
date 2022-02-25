bf3720f6f3561a834325f5ea008ef0eda54e0963
Xayah <zds1249475336@gmail.com>
2022-02-25 16:55:44 +0800

    feat(lib): Create transfer class

---
+++ MaterialYouFileExplorer/src/main/java/com/xayah/materialyoufileexplorer/MaterialYouFileExplorer.kt
@@ -0,0 +1,20 @@
+package com.xayah.materialyoufileexplorer
+
+import android.content.Intent
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+
+class MaterialYouFileExplorer {
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    fun initialize(activity: ComponentActivity, callback: (path: String) -> Unit) {
+        activityResultLauncher =
+            activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                callback(it.data?.getStringExtra("path") ?: "")
+            }
+    }
+
+    fun toExplorer(activity: ComponentActivity) {
+        activityResultLauncher.launch(Intent(activity, ExplorerActivity::class.java))
+    }
+}
