1f8a4c82d3501ea2595b33f0eb15b5e5bd2a5660
Xayah <zds1249475336@gmail.com>
2022-02-25 16:56:32 +0800

    feat(sample): Update transfer functions

--- app/src/main/java/com/xayah/sample/materialyoufileexplorer/MainActivity.kt
+++ app/src/main/java/com/xayah/sample/materialyoufileexplorer/MainActivity.kt
@@ -1,31 +1,23 @@
 package com.xayah.sample.materialyoufileexplorer
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import com.xayah.materialyoufileexplorer.ExplorerActivity
+import com.xayah.materialyoufileexplorer.MaterialYouFileExplorer
 import com.xayah.sample.materialyoufileexplorer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        activityResultLauncher =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-                val path = it.data?.getStringExtra("path")
-                if (path != null) {
-                    binding.textInputEditText.setText(path)
-                }
-            }
+        val materialYouFileExplorer = MaterialYouFileExplorer()
+        materialYouFileExplorer.initialize(this) {
+            binding.textInputEditText.setText(it)
+        }
 
         binding.filledButton.setOnClickListener {
-            activityResultLauncher.launch(Intent(this, ExplorerActivity::class.java))
+            materialYouFileExplorer.toExplorer(this)
         }
     }
 }
