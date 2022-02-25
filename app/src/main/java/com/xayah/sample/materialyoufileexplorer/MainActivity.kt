8f05550c590baa4c839086ee5d8d3b9d2305df28
Xayah <zds1249475336@gmail.com>
2022-02-25 16:37:47 +0800

    feat(sample): Initial sample

--- app/src/main/java/com/xayah/sample/materialyoufileexplorer/MainActivity.kt
+++ app/src/main/java/com/xayah/sample/materialyoufileexplorer/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.xayah.sample.materialyoufileexplorer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.xayah.materialyoufileexplorer.ExplorerActivity
+import com.xayah.sample.materialyoufileexplorer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                val path = it.data?.getStringExtra("path")
+                if (path != null) {
+                    binding.textInputEditText.setText(path)
+                }
+            }
+
+        binding.filledButton.setOnClickListener {
+            activityResultLauncher.launch(Intent(this, ExplorerActivity::class.java))
+        }
     }
 }
