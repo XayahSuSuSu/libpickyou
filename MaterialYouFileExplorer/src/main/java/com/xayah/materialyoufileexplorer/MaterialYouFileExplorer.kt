1d129e22df56750e4ffdc77b5661dbe92cc26718
Xayah <zds1249475336@gmail.com>
2022-02-26 13:53:56 +0800

    feat: Support for fragment

--- MaterialYouFileExplorer/src/main/java/com/xayah/materialyoufileexplorer/MaterialYouFileExplorer.kt
+++ MaterialYouFileExplorer/src/main/java/com/xayah/materialyoufileexplorer/MaterialYouFileExplorer.kt
@@ -1,9 +1,11 @@
 package com.xayah.materialyoufileexplorer
 
+import android.content.Context
 import android.content.Intent
 import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
 
 class MaterialYouFileExplorer {
     private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
@@ -14,10 +16,17 @@ class MaterialYouFileExplorer {
             }
     }
 
-    fun toExplorer(activity: ComponentActivity, isFile: Boolean) {
+    fun initialize(fragment: Fragment, callback: (path: String) -> Unit) {
+        activityResultLauncher =
+            fragment.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                callback(it.data?.getStringExtra("path") ?: "")
+            }
+    }
+
+    fun toExplorer(context: Context, isFile: Boolean) {
         activityResultLauncher.launch(
             Intent(
-                activity,
+                context,
                 ExplorerActivity::class.java
             ).putExtra("isFile", isFile)
         )
