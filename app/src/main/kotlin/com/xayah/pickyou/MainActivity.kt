bfd9d55cebe18f9d4b6832fee0c1ff8da4384e32
Xayah <zds1249475336@gmail.com>
2024-01-02 18:34:59 +0800

    feat: Update sample
    Change-Id: I3e736ed1f5ff16783d178e638951cdd6ae0e0872

--- app/src/main/kotlin/com/xayah/pickyou/MainActivity.kt
+++ app/src/main/kotlin/com/xayah/pickyou/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.xayah.pickyou
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -25,37 +32,58 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.xayah.libpickyou.ui.PickYouLauncher
+import com.xayah.libpickyou.ui.model.PermissionType
 import com.xayah.libpickyou.ui.model.PickerType
 import com.xayah.pickyou.ui.theme.PickYouTheme
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private val launcher = PickYouLauncher()
 
-    private fun launch(path: String, type: PickerType, number: String, title: String, pathPrefixHiddenNum: String) {
+    private fun SnackbarHostState.launch(
+        path: String,
+        type: PickerType,
+        number: String,
+        title: String,
+        pathPrefixHiddenNum: String,
+        permissionType: PermissionType,
+        scope: CoroutineScope,
+    ) {
         val num = number.toIntOrNull()
         val hiddenNum = pathPrefixHiddenNum.toIntOrNull()
         if (num == null || hiddenNum == null) {
-            Toast.makeText(this@MainActivity, "Please type number", Toast.LENGTH_SHORT)
-                .show()
+            scope.launch {
+                showSnackbar("Please type number")
+            }
         } else {
             launcher.setDefaultPath(path)
             launcher.setType(type)
             launcher.setLimitation(num)
             launcher.setTitle(title)
             launcher.setPathPrefixHiddenNum(hiddenNum)
+            launcher.setPermissionType(permissionType)
             launcher.launch(this@MainActivity) { path ->
-                Toast.makeText(this@MainActivity, path.toString(), Toast.LENGTH_SHORT)
-                    .show()
+                scope.launch {
+                    showSnackbar(path.toString())
+                }
             }
         }
     }
 
-    private suspend fun SnackbarHostState.launch(path: String, type: PickerType, number: String, title: String, pathPrefixHiddenNum: String) {
+    private suspend fun SnackbarHostState.launchSuspended(
+        path: String,
+        type: PickerType,
+        number: String,
+        title: String,
+        pathPrefixHiddenNum: String,
+        permissionType: PermissionType,
+    ) {
         val num = number.toIntOrNull()
         val hiddenNum = pathPrefixHiddenNum.toIntOrNull()
         if (num == null || hiddenNum == null) {
@@ -66,6 +94,7 @@ class MainActivity : ComponentActivity() {
             launcher.setLimitation(num)
             launcher.setTitle(title)
             launcher.setPathPrefixHiddenNum(hiddenNum)
+            launcher.setPermissionType(permissionType)
             val path = launcher.awaitPickerOnce(this@MainActivity)
             showSnackbar(path.toString())
         }
@@ -95,6 +124,7 @@ class MainActivity : ComponentActivity() {
                     },
                 ) { innerPadding ->
                     Column(
+                        modifier = Modifier.verticalScroll(rememberScrollState()),
                         horizontalAlignment = Alignment.CenterHorizontally,
                         verticalArrangement = Arrangement.spacedBy(16.dp)
                     ) {
@@ -133,20 +163,53 @@ class MainActivity : ComponentActivity() {
                             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
                         )
 
+                        var permissionType by remember { mutableStateOf(PermissionType.NORMAL) }
+                        val options = remember {
+                            listOf("Normal mode" to PermissionType.NORMAL, "Root mode" to PermissionType.ROOT)
+                        }
+                        Column(modifier = Modifier.selectableGroup()) {
+                            options.forEach { item ->
+                                Row(
+                                    Modifier
+                                        .fillMaxWidth()
+                                        .height(56.dp)
+                                        .selectable(
+                                            selected = (item.second == permissionType),
+                                            onClick = {
+                                                permissionType = item.second
+                                            },
+                                            role = Role.RadioButton
+                                        )
+                                        .padding(horizontal = 16.dp),
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    RadioButton(
+                                        selected = (item.second == permissionType),
+                                        onClick = null
+                                    )
+                                    Text(
+                                        text = item.first,
+                                        style = MaterialTheme.typography.bodyLarge,
+                                        modifier = Modifier.padding(start = 16.dp)
+                                    )
+                                }
+                            }
+                        }
+
                         Button(
                             onClick = {
-                                launch(defaultPath, PickerType.FILE, number, title, pathPrefixHiddenNum)
+                                snackbarHostState.launch(defaultPath, PickerType.FILE, number, title, pathPrefixHiddenNum, permissionType, coroutineScope)
                             }, content = { Text(text = "Pick file") }
                         )
                         Button(
                             onClick = {
-                                launch(defaultPath, PickerType.DIRECTORY, number, title, pathPrefixHiddenNum)
+                                snackbarHostState.launch(defaultPath, PickerType.DIRECTORY, number, title, pathPrefixHiddenNum, permissionType, coroutineScope)
                             },
                             content = { Text(text = "Pick directory") }
                         )
                         Button(
                             onClick = {
-                                launch(defaultPath, PickerType.BOTH, number, title, pathPrefixHiddenNum)
+                                snackbarHostState.launch(defaultPath, PickerType.BOTH, number, title, pathPrefixHiddenNum, permissionType, coroutineScope)
                             },
                             content = { Text(text = "Pick file or directory") }
                         )
@@ -154,7 +217,7 @@ class MainActivity : ComponentActivity() {
                             onClick = {
                                 coroutineScope.launch {
                                     runCatching {
-                                        snackbarHostState.launch(defaultPath, PickerType.BOTH, number, title, pathPrefixHiddenNum)
+                                        snackbarHostState.launchSuspended(defaultPath, PickerType.BOTH, number, title, pathPrefixHiddenNum, permissionType)
                                     }.onFailure {
                                         snackbarHostState.showSnackbar(it.message.orEmpty())
                                     }
