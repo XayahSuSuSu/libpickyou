68fb4e2b5d30554c3af2c5c9ae3aaeaa24675d14
Xayah <zds1249475336@gmail.com>
2024-01-02 17:50:14 +0800

    perf: Update UI of Checkbox
    Change-Id: I3196c09878996cb69147bcd7a08cd53124cb3564

--- libpickyou/src/main/kotlin/com/xayah/libpickyou/ui/components/ListItem.kt
+++ libpickyou/src/main/kotlin/com/xayah/libpickyou/ui/components/ListItem.kt
@@ -7,8 +7,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -17,6 +18,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import com.xayah.libpickyou.R
+import com.xayah.libpickyou.ui.model.ImageVectorToken
+import com.xayah.libpickyou.ui.model.fromDrawable
+import com.xayah.libpickyou.ui.model.value
 import com.xayah.libpickyou.ui.tokens.ChildListItemTokens
 
 @Composable
@@ -61,13 +65,22 @@ internal fun ChildListItem(
                 if (subtitle != null)
                     BodyMediumText(text = subtitle)
             }
-            if (isChecked != null && onCheckBoxClick != null)
-                Checkbox(
+            if (isChecked != null && onCheckBoxClick != null) {
+                IconToggleButton(
                     checked = isChecked.value,
-                    onCheckedChange = {
-                        onCheckBoxClick(isChecked.value.not())
+                    onCheckedChange = { onCheckBoxClick(isChecked.value.not()) }
+                ) {
+                    if (isChecked.value) {
+                        Icon(ImageVectorToken.fromDrawable(R.drawable.ic_rounded_check_circle).value, contentDescription = null)
+                    } else {
+                        Icon(
+                            ImageVectorToken.fromDrawable(R.drawable.ic_rounded_unchecked_circle).value,
+                            tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f),
+                            contentDescription = null
+                        )
                     }
-                )
+                }
+            }
         }
     }
 }
